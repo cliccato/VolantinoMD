4c4f2612fe17ff2ea99d11d3d80fb99f9ac8d170
iltiaaa <califano05@gmail.com>
2022-01-27 22:25:25 +0100

    add volantino.sh

---
+++ volantino.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+currentdate=`date +"%Y-%m-%d"`
+url="https://mdspa2.cdn.hugecloud.eu/upload/presente/volantini/nord-p-g/volantino.pdf"
+name="volantino".$currentdate".pdf"
+
+curl $url -o $name -s
